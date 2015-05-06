7a8c49777ffef4a4a25c01ef9b3aa640c244ebb6
bitsculpt <davew1982@gmail.com>
2015-05-06 18:41:19 +0000

    fixing up some views, adding a little more functionality to the app

--- grillingapp/app/controllers/recipes_controller.rb
+++ grillingapp/app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
     before_action :find_recipe, only: [:show, :edit, :update, :destroy]
     def index
+        @recipe = Recipe.all.order("created_at DESC")
     end
     
     def show
@@ -9,6 +10,12 @@ class RecipesController < ApplicationController
     
     def create
         @recipe = Recipe.new(recipe_params)
+        
+        if @recipe.save
+            redirect_to @recipe, notice: "Successfully created a new recipe."
+        else
+            render "new"
+        end
     end
     
     def new
@@ -16,10 +23,30 @@ class RecipesController < ApplicationController
     end
    
    def recipe_params
-       params.require(:recipe.permit(:title, :description))
+       params.require(:recipe).permit(:title, :description)
    end
-private
-def find_recipe
-    @recipe = Recipe.find(params[:id])
-end   
+
+
+    def edit
+    end
+    
+    def update
+        if @recipe.update(recipe_params)
+            redirect_to @recipe
+        else
+            render "edit"
+        end
+        
+    end
+    
+    def destroy
+        @recipe.destroy
+        redirect_to root_path, notice: "Deleted Recipe"
+    end
+    
+    
+    private
+    def find_recipe
+        @recipe = Recipe.find(params[:id])
+    end   
 end
